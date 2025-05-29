55ef38460ee2b83d778154aa5a82e1f1357605e7
Rochelle Awuor <awuorochelle75@gmail.com>
2025-05-29 08:33:07 +0300

    modifies files

--- create_db.py
+++ create_db.py
@@ -1,10 +1,10 @@
 from lib.db.models import Base  # your declarative Base with models
 from sqlalchemy import create_engine
 
-# Create the engine with the same path you use in Alembic and your app
+# Creates the engine with the same path you use in Alembic and your app
 engine = create_engine('sqlite:///lib/db/app.db')
 
-# Create all tables in the database (if they don't already exist)
+# Creates all tables in the database (if they don't already exist)
 Base.metadata.create_all(engine)
 
 print("Database file and tables created successfully!")
